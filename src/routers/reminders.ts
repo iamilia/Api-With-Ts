239510dbd0b008c61907c6964b09712fc248069f
ilia <ilialotfi1083@gmail.com>
2022-11-06 20:44:45 +0330

    reminder get method from db

--- src/routers/reminders.ts
+++ src/routers/reminders.ts
@@ -5,7 +5,9 @@ import Reminder from "../models/reminder";
 const router  = Router();
 const storage : Reminder[] = []
 router.get("/", (req , res) => {
-    res.json(storage)
+    Execute<Object, null>("SELECT * FROM `test`.`reminder` LIMIT 1000;", null).then(v => {
+        res.status(201).json(v)
+    }).catch(err => console.error(err)); // Here You Can Check your Storage
 })
 router.post("/", (req, res) =>{
     const {titel} = req.body as ProfileCreateDto
