acb7df649c8ac3fb846ec23f8168362a1d840740
ilia <ilialotfi1083@gmail.com>
2022-11-06 20:50:32 +0330

    Update mysql.ts
    end createConnection to donot time out

--- src/models/mysql.ts
+++ src/models/mysql.ts
@@ -4,16 +4,17 @@ import * as mysql from 'mysql2/promise';
 export async function Execute<T, T2>(sql : string, param : T2) {
     let err: any
     let __rows: mysql.FieldPacket[]
-    let connection = await mysql.createConnection({
+    await mysql.createConnection({
         host: 'localhost',
         user: 'root',
         database: 'test'
-    }).then(conn => conn.query(sql, param))
-    .then(([rows, fields]) => {
-        __rows = fields
-        err = rows
-    } 
-    );
+    }).then(async(conn) => {
+        await conn.query(sql, param).then(([rows, fields]) => {
+            __rows = fields
+            err = rows
+        });
+        conn.end()
+    })
     return await new Promise<T>((reject, resolve) => {
         if (err) return reject(err)
         resolve(__rows)
